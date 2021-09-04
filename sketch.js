7212fd21c195973c17e2d6e66c91f0244ba51c46
DiyaNagesia <73646449+DiyaNagesia@users.noreply.github.com>
2021-09-04 15:06:45 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var spaceShip1,spaceShip1Image;
+var spaceShip2,spaceShip2Image;
+var spaceShip3,spaceShip3Image
+var spaceCraft1,spaceCraft1Image;
+var spaceCraft2,spaceCraft2Image;
+var spaceCraft3,spaceCraft3Image;
+var spaceCraft4,spaceCraft4Image;
+var spaceCraft5,spaceCraft5Image;
+var aliens,aliensImage;
+var comets,cometsImage;
+var diamonds,diamondsImage;
+
+//function preload(){
+
+//}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+    var spaceShip1 = createSprite(width/2,height/2)
+}
+function draw(){
+    background("white")
+    drawSprites()
+}
